89675c5ab460d53234f2c91a6be3271b343aa31e
0xC0LD <coldmailgg@gmail.com>
2022-04-29 16:09:39 +0200

    cry2

--- .vip/tools/cry/main.c
+++ .vip/tools/cry/main.c
@@ -15,6 +15,12 @@ char* strremove(char* str, const char* sub) {
 	return str;
 }
 
+const char* ext(const char* filename) {
+    const char* dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
 int main(int argc, char** argv) {
 
 	// check args
@@ -28,61 +34,42 @@ int main(int argc, char** argv) {
 	FILE* fp1 = fopen(filepath, "rb");
 	if (!fp1) { fprintf(stderr, "Failed to open file: %s\n", filepath); return 1; }
 
+	// check filename ext
+	int dec = 0;
+	if (strstr(ext(filepath), "cry")) { dec = 1; printf("DECRYPTION MODE\n"); }
+	else                                            {          printf("ENCRYPTION MODE\n"); }
+
 	// get password
 	printf("PASSWORD: ");
 	char pass[KSIZ] = {0};
 	fgets(pass, KSIZ-1, stdin);
 	int passlen = strlen(pass);
 
-	if (strstr(filepath, ".cry")) {
-		// make new filename
-		char* filepath2 = (char*)calloc(passlen + 1, sizeof(char));
-		strcpy(filepath2, filepath);
-		strremove(filepath2, ".cry");
+	// make new filename for output
+	char* filepath2 = (char*)calloc(passlen + 1, sizeof(char));
+	if (filepath2 == NULL) { fprintf(stderr, "error: failed to calloc name for output file\n"); return 1; }
+	strcpy(filepath2, filepath);
+	if (dec) { strremove(filepath2, ".cry"); }
+	else     { strcat   (filepath2, ".cry"); }
 
-		// open newfile and decrypt
-		FILE* fp2 = fopen(filepath2, "wb");
-		if (!fp1) { fprintf(stderr, "Failed to open file: %s\n", filepath2); return 1; }
-		int c;
-		int i = 0;
-		while ((c = fgetc(fp1)) != EOF) {
-			int g = c - pass[i];
-			fputc(g, fp2);
-			i++;
-			if (i > passlen-1) { i = 0; }
-		}
+	// open output file
+	FILE* fp2 = fopen(filepath2, "wb");
+	if (!fp1) { fprintf(stderr, "failed to open output file: %s\n", filepath2); return 1; }
 
-		// cleanup
-		free(filepath2);
-		fclose(fp1);
-		fclose(fp2);
-		return 0;
-	}
-	else {
-		// make new filename
-		char* filepath2 = (char*)calloc(passlen + 1, sizeof(char));
-		strcpy(filepath2, filepath);
-		strcat(filepath2, ".cry");
-
-		FILE* fp2 = fopen(filepath2, "wb");
-		if (!fp1) { fprintf(stderr, "Failed to open file: %s\n", filepath2); return 1; }
-		int c;
-		int i = 0;
-		while ((c = fgetc(fp1)) != EOF) {
-			int g = c + pass[i];
-			fputc(g, fp2);
-			i++;
-			if (i > passlen-1) { i = 0; }
-		}
-
-		// cleanup
-		free(filepath2);
-		fclose(fp1);
-		fclose(fp2);
-		return 0;
+	// cry algorithm
+	int c;
+	int i = 0;
+	while ((c = fgetc(fp1)) != EOF) {
+		int g = dec ? c - pass[i] : c + pass[i];
+		fputc(g, fp2);
+		i++;
+		if (i > passlen-1) { i = 0; }
 	}
 
-
+	// cleanup
+	free(filepath2);
+	fclose(fp1);
+	fclose(fp2);
 
 	return 0;
 }
