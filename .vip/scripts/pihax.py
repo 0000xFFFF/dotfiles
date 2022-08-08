5c3951b8accb06f0d9c84a6326aaec2c0e7aa8fc
0xC0LD <coldmailgg@gmail.com>
2022-08-08 16:32:23 +0200

    removed tabs

--- .vip/scripts/pihax.py
+++ .vip/scripts/pihax.py
@@ -38,11 +38,11 @@ KEY3_PIN      = 16
 def checkRoot():
     return os.geteuid() == 0
 def term_clear():
-	os.system("clear")
+    os.system("clear")
 def term_cursor_reset():
     print("\033[0;0H", end='')
 def term_cursor_hide():
-	print("\033[?25l")
+    print("\033[?25l")
 def term_cursor_show():
     print("\033[?25h")
 
