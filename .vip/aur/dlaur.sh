c0654cdba564a8c58fecd954076568990d8bb08b
0000xFFFF <coldmailgg@gmail.com>
2024-08-08 18:14:29 +0200

    dlaur

--- .vip/aur/dlaur.sh
+++ .vip/aur/dlaur.sh
@@ -1,8 +1,12 @@
-git clone https://aur.archlinux.org/radeon-profile-git.git
+git clone https://aur.archlinux.org/adwaita-qt-git.git
+git clone https://aur.archlinux.org/amdgpu-pro-installer.git
+git clone https://aur.archlinux.org/code-features.git
+git clone https://aur.archlinux.org/gqrx.git
+git clone https://aur.archlinux.org/mosint.git
+git clone https://aur.archlinux.org/netdiscover.git
+git clone https://aur.archlinux.org/nexfil.git
 git clone https://aur.archlinux.org/nomacs.git
 git clone https://aur.archlinux.org/qdirstat.git
 git clone https://aur.archlinux.org/qt5-styleplugins.git
+git clone https://aur.archlinux.org/radeon-profile-git.git
 git clone https://aur.archlinux.org/rar.git
-git clone https://aur.archlinux.org/code-features.git
-git clone https://aur.archlinux.org/amdgpu-pro-installer.git
-git clone https://aur.archlinux.org/netdiscover.git
