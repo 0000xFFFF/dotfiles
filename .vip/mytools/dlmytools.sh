a35f859539702ad1540ea3b078c975c36e756860
0000xFFFF <coldmailgg@gmail.com>
2024-09-10 20:50:03 +0200

    added more mytools

--- .vip/mytools/dlmytools.sh
+++ .vip/mytools/dlmytools.sh
@@ -2,4 +2,5 @@
 git clone https://github.com/0000xFFFF/dumpit
 git clone https://github.com/0000xFFFF/L3MON
 git clone https://github.com/0000xFFFF/hcx-scripts
-
+git clone https://github.com/0000xFFFF/silentripper
+git clone https://github.com/0000xFFFF/4chan-dl
