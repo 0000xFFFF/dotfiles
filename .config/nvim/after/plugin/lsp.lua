3348151ca596f4e4b1aa3b3e02285cb6ac688f64
0000xFFFF <coldmailgg@gmail.com>
2024-09-14 21:52:15 +0200

    lsp lua overwrite bindings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -44,7 +44,7 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = {buffer = bufnr, remap = true}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
