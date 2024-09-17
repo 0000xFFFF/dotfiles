116f4061b80e39afbb444c795c5125d98028b29a
0000xFFFF <coldmailgg@gmail.com>
2024-09-17 23:39:19 +0200

    i3screen fix

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1920x1200 --pos 1280x0 --rotate normal --output DisplayPort-1 --mode 1280x1024 --pos 0x176 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 3200x120 --rotate normal --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-2 --mode 1280x1024 --pos 3840x0 --rotate normal --output HDMI-A-0 --off
