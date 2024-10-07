e2508a3400a716ceee988fcd190eb3d5878c411c
0000xFFFF <coldmailgg@gmail.com>
2024-10-07 21:00:48 +0200

    added more mytools

--- .vip/mytools/dlmytools.sh
+++ .vip/mytools/dlmytools.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+git clone https://github.com/0000xFFFF/4chan-dl
+git clone https://github.com/0000xFFFF/cnn-emnist-cam
 git clone https://github.com/0000xFFFF/dumpit
-git clone https://github.com/0000xFFFF/L3MON
+git clone https://github.com/0000xFFFF/first-order-model
+git clone https://github.com/0000xFFFF/gridview
 git clone https://github.com/0000xFFFF/hcx-fastgenlst
 git clone https://github.com/0000xFFFF/hcx-scripts
+git clone https://github.com/0000xFFFF/L3MON
 git clone https://github.com/0000xFFFF/silentripper
-git clone https://github.com/0000xFFFF/4chan-dl
