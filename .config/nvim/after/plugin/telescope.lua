8316ce3c1e6514f7923c9630c45807b38fc31619
0000xFFFF <coldmailgg@gmail.com>
2025-05-26 09:47:14 +0200

    telescope live grep fix

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pg', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set('n', '<leader>pp', builtin.resume, {})
