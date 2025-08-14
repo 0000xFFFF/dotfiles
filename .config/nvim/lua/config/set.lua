c32d63ec3f7810853b77983c88a05d73a92b693d
0000xFFFF <coldmailgg@gmail.com>
2025-08-15 01:33:21 +0200

    added nvim dicts for autocomplete in insert mode CTRL+X CTRL+K

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -17,6 +17,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
+vim.opt.dictionary = {"~/.config/nvim/dicts/english.sh", "~/.config/nvim/dicts/serbian.txt"}
 vim.g.mapleader = " "
 
 vim.opt.list = true
