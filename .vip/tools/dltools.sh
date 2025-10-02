2a84d375af53289a4cfb8064dae76b40cdbc06b6
0000xFFFF <coldmailgg@gmail.com>
2025-10-02 20:42:02 +0200

    more tools

--- .vip/tools/dltools.sh
+++ .vip/tools/dltools.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-git clone https://github.com/nimaipatel/brn
-git clone https://github.com/qarmin/czkawka
-git clone https://github.com/nil0x42/duplicut
 git clone https://github.com/AltraMayor/f3
+git clone https://github.com/cesanta/mongoose
+git clone https://github.com/cmuratori/termbench
 git clone https://github.com/gqrx-sdr/gqrx
-git clone https://github.com/maldevel/IPGeoLocation
-git clone https://github.com/k4m4/kickthemout
 git clone https://github.com/JonahGrimm/Meisunry
-git clone https://github.com/thewhiteh4t/nexfil
-git clone https://github.com/RUB-NDS/PRET
+git clone https://github.com/k4m4/kickthemout
+git clone https://github.com/lipnitsk/mocp
+git clone https://github.com/maldevel/IPGeoLocation
+git clone https://github.com/nil0x42/duplicut
+git clone https://github.com/nimaipatel/brn
+git clone https://github.com/nonpop/xkblayout-state
+git clone https://github.com/osmocom/gr-osmosdr
 git clone https://github.com/pwnieexpress/pwn_plug_sources
+git clone https://github.com/qarmin/czkawka
+git clone https://github.com/RUB-NDS/PRET
+git clone https://github.com/thewhiteh4t/nexfil
 git clone https://github.com/threat9/routersploit
 git clone https://github.com/xmendez/wfuzz
-git clone https://github.com/nonpop/xkblayout-state
