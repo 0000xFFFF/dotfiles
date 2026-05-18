6d8736f16d767d26f6e48850f98bff45cafce8d8
0000xFFFF <coldmailgg@gmail.com>
2026-05-18 15:32:10 +0200

    remove telescope version tag

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -78,7 +78,6 @@ return {
     -- Telescope
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             require("config.telescope")
